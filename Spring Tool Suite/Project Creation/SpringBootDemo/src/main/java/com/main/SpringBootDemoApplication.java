9ac0d78559a5760a18fd787122e6ca91e8c7d723
shikalgar2002 <126649250+shikalgar2002@users.noreply.github.com>
2025-01-24 09:35:27 +0530

    Add files via upload

---
+++ Spring Tool Suite/Project Creation/SpringBootDemo/src/main/java/com/main/SpringBootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+		System.out.println("Application is Started");
+	}
+
+}
